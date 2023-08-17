e8d10550f2da0b38affa72015cad3f58353a345a
Ashlok Chaudhary <116460151+Ashlok2003@users.noreply.github.com>
2023-08-17 21:43:52 +0530

    Update ashlok.cpp

--- ashlok.cpp
+++ ashlok.cpp
@@ -1,8 +1,29 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
-//hello
-int main(){
-    cout<<"Git File\n";
-    cout<<"Hello Ashlok Chaudhary\n";
+class Life {
+public:
+    Life(const std::string& message) {
+        std::cout << message << std::endl;
+    }
+};
+
+int main() {
+    std::string messages[] = {
+        "Life is full of Hard Working",
+        "You must never give up",
+        "Fight till your last breadth",
+        "Always ask youself why you start this",
+        "made mistakes but always tend to learn something."
+    };
+
+    int numMessages = sizeof(messages) / sizeof(messages[0]);
+
+    while (true) {
+        for (int i = 0; i < numMessages; ++i) {
+            Life life(messages[i]);
+            std::this_thread::sleep_for(std::chrono::seconds(3));
+        }
+    }
+
     return 0;
 }
