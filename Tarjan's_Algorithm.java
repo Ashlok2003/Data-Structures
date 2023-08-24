97b43f2f43f7d71722028489ee2c21d489493d14
Ashlok Chaudhary <116460151+Ashlok2003@users.noreply.github.com>
2023-08-25 02:32:22 +0530

    Update Tarjan's_Algorithm.java
    Updation

--- Tarjan's_Algorithm.java
+++ Tarjan's_Algorithm.java
@@ -1,5 +1,33 @@
-// Following Code is basically The solution of LEETCODE Question: Critical Connections in a Network
-// The Tarjan's Algorithm is used to find the bridges in an graph using DFS.
+/*
+=======================================================================================================================================================
+                                            Algorithm Intutution & Implementation
+=======================================================================================================================================================
+The algorithm is based on the idea that a bridge is an edge that, if removed, will disconnect the graph into two or more disjoint components.
+
+The code first constructs an adjacency list from the input list of connections con. It then initializes some data structures used in the algorithm,
+such as visited, tin, low, and brides. (tin = inertion time timer basically).
+
+The algorithm starts by visiting each node in the graph, and for each node node, it performs a depth-first search 
+to find all the nodes that can be reached from node while avoiding cycles. In this search, timer is used to keep track
+of the time when each node is first visited, disc[node], and low[node] is the lowest disc value that can be reached from node via a back edge.
+
+If a neighbor neigh of node has not been visited before, the search continues recursively from neigh, and low[node] is updated 
+with the minimum of low[node] and low[neigh]. If low[neigh] > tinc[node], then the edge from node to neigh is a bridge, and it is added to the res vector.
+
+If neigh has already been visited and is not the parent of node, then it is a back edge, and low[node] is updated with 
+the minimum of low[node] and tin[neigh].
+
+Time complexity: The algorithm performs a depth-first search starting from each node in the graph. 
+Since each edge is traversed at most twice (once for each endpoint), the time complexity of the algorithm is O(V+E), 
+where V is the number of vertices and E is the number of edges in the graph.
+
+Space complexity: The space complexity is O(V+E), where V is the number of vertices and E is the number of edges in the graph. 
+This is because the algorithm uses the adjacency list to store the graph, and it also uses some additional data structures, 
+such as visited, tin, low, and bridges, which all have size O(V).
+===========================================================================================================================================================
+*/
+// Following Code is basically The solution of LEETCODE Question: Critical Connections in a Network 
+//                which use Trajan's Algorithm to find the critical edges coordinates. :)
 class Solution {
     private int Timer = 1;
     public void DFS(int node, int parent, ArrayList<Boolean> visited, ArrayList<ArrayList<Integer>> graph, int[] tin, int[] low, List<List<Integer>> bridges){
@@ -47,3 +75,4 @@ class Solution {
         return bridges;
     }
 }
+// Thanks for Reading and Checking out............................. :)
